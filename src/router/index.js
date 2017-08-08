3e2e7dd5747c06c393a93c9cf2bd67c7a408e9cb
axstone-zhang <1017795375@qq.com>
2017-08-08 18:06:05 +0800

    detail page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import IndexPage from '@/pages/index'
+import DetialPage from '@/pages/detial'
+import ForecastPage from '@/pages/detial/forecast'
+import CountPage from '@/pages/detial/count'
+import PublishPage from '@/pages/detial/publish'
+import AnalysisPage from '@/pages/detial/analysis'
 
 Vue.use(VueRouter)
 
@@ -11,6 +16,28 @@ export default  new VueRouter({
 		{
 			path: '/',
 			component: IndexPage
+		},{
+			path: '/detial',
+			component: DetialPage,
+			redirect: '/detial/analysis',
+			children: [
+				{
+					path: 'forecast',
+					component: ForecastPage
+				},
+				{
+					path: 'count',
+					component: CountPage
+				},
+				{
+					path: 'publish',
+					component: PublishPage
+				},
+				{
+					path: 'analysis',
+					component: AnalysisPage
+				}
+			]
 		}
 		
 	]
